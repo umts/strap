c5437107db0ed198e213a9f3986c5f1d3c86b339
Matt Moretti <werebus@gmail.com>
2019-03-07 15:33:55 -0500

    Use "touch" mode for Passenger

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,6 @@
 set :default_env, { PATH: '/opt/rubies/ruby-2.6.1/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' }
+set :passenger_restart_with_touch, true
+
 remote_user = Net::SSH::Config.for('af-transit-app4.admin.umass.edu')[:user] || ENV['USER']
 server 'af-transit-app4.admin.umass.edu',
        roles: %w[app web],
