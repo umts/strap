3560abf06574b4165b871cf5ee31c3cb03be3c88
Matt Moretti <werebus@gmail.com>
2024-11-20 23:56:23 -0500

    Remove a few unneeded Capistrano configs

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,3 @@
-set :default_env, { PATH: '/opt/ruby/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' }
 set :passenger_restart_with_touch, true
 
-remote_user = Net::SSH::Config.for('af-transit-app4.admin.umass.edu')[:user] || ENV['USER']
-server 'af-transit-app4.admin.umass.edu',
-       roles: %w[app web],
-       user: remote_user
+server 'af-transit-app4.admin.umass.edu', roles: %w[app web]
