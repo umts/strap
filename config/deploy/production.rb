9e3cd494232fbbe908d6d21bfc6e7f383e7f0235
Matt Moretti <werebus@gmail.com>
2024-11-20 20:19:08 -0500

    Rubocop fixes
    I decided to switch to single quotes, but I left the other upstream
    preferences for now. Future PRs, probably.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 set :default_env, { PATH: '/opt/ruby/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' }
 set :passenger_restart_with_touch, true
 
-remote_user = Net::SSH::Config.for('af-transit-app4.admin.umass.edu')[:user] || ENV['USER']
+remote_user = Net::SSH::Config.for('af-transit-app4.admin.umass.edu')[:user] || ENV.fetch('USER', nil)
 server 'af-transit-app4.admin.umass.edu',
        roles: %w[app web],
-       user: remote_user
+       user:  remote_user
