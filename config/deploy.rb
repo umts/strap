9f876be22d701e19edb0f9b9c4198b1c9fff9816
Matt Moretti <werebus@gmail.com>
2024-11-22 13:01:20 -0500

    Remove rubocop exceptions for hash and `end` align

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,9 +19,9 @@ before 'git:check', 'git:allow_shared'
 namespace :git do
   desc 'Allow use of shared git repository'
   task :allow_shared do
-    on release_roles(:all), in:    :groups,
+    on release_roles(:all), in: :groups,
                             limit: fetch(:git_max_concurrent_connections),
-                            wait:  fetch(:git_wait_interval) do
+                            wait: fetch(:git_wait_interval) do
       with fetch(:git_environmental_variables) do
         execute :git, :config, '--global', '--replace-all', 'safe.directory', repo_path, repo_path
       end
